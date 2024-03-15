b535f1f76dde17f569ac06b9e894a0e69e533481
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2024-03-15 11:37:00 +0000

    install tailwindcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,17 +3,16 @@ const path = require('path')
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    'storybook-css-modules',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     {
       name: '@storybook/addon-postcss',
       options: {
         postcssLoaderOptions: {
-          implementation: require('postcss')
-        }
-      }
-    }
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import './preview.css'
 
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
